6c9d31aec4754461700aaa4ec62d7e72fc020d1b
Ben Orchard <thefirstmuffinman@gmail.com>
2024-03-14 21:33:50 +0000

    FRK2: cleanup

--- src/FrkPs2/FRK2.hs
+++ src/FrkPs2/FRK2.hs
@@ -1,8 +1,10 @@
-{-
-Coding is really, really slow and I don't know why.
-Coding cmn.bin (2.9MB) takes ~15s.
-Surely this is wrong.
-Disabling MT19937 makes it ~2s. Still slow.
+{- | The "FRK2" encoded archive format that Fuuraiki (2006) uses.
+
+Data is encoded using a constant-seed MT19937 (Mersenne Twister).
+Conveniently, this means the same operation both encodes and decodes. Indeed:
+
+    code (code x) === x
+
 -}
 
 module FrkPs2.FRK2 where
@@ -24,6 +26,9 @@ import Data.ByteString          qualified as B
 import System.IO ( Handle, hGetBuf, hPutBuf )
 import Foreign.Marshal.Alloc ( allocaBytes )
 
+initialPrng :: MT19937
+initialPrng = MT19937.skip 1 $ MT19937.init 0xFACEFACE
+
 -- | Encodes or decodes.
 --
 -- @code rng w (fst (code rng w)) === w@
@@ -39,16 +44,14 @@ code :: B.ByteString -> B.ByteString
 code (B.BS fptr len) =
     B.unsafeCreate len $ \dest -> unsafeWithForeignPtr fptr $ \src -> do
         copyBytes dest src len
-        _prng' <- code' dest len prng
+        _prng' <- code' dest len initialPrng
         pure ()
-  where
-    prng = MT19937.skip 1 $ MT19937.init 0xFACEFACE
 
 codeFile :: FilePath -> IO B.ByteString
 codeFile fp = code <$> B.readFile fp
 
 codeHandle :: Int -> Handle -> Handle -> IO ()
-codeHandle bufLen hr hw = allocaBytes bufLen (go prng0)
+codeHandle bufLen hr hw = allocaBytes bufLen (go initialPrng)
   where
     go :: MT19937 -> Ptr Word8 -> IO ()
     go prng buf = do
@@ -61,7 +64,6 @@ codeHandle bufLen hr hw = allocaBytes bufLen (go prng0)
                 -- safely assume bufLen == bufLenWritten so either is OK
                 hPutBuf hw buf bufLenWritten
                 go prng' buf
-    prng0 = MT19937.skip 1 $ MT19937.init 0xFACEFACE
 
 -- buf is mutable data
 -- taking Ptr because looks like copy prims work on them (see GHC.IO.Handle)
@@ -82,59 +84,3 @@ code' buf len prng0 = goW32 prng0 0
             writeOffPtr bufW32 i w32Coded
             goW32 prng' (i+1)
     goW8s prng _i = pure prng -- TODO idk how to handle these exactly
-
-{-
-
-type instance DispatchOf Coder = Dynamic
-data Coder :: Effect where
-    Finished    :: Coder m Bool
-    NextBlock   :: Coder m ByteString
-    NextMT19937 :: Coder m Word32
-    WriteBlock  :: ByteString -> Coder m ()
-
-nextMT19937 :: (HasCallStack, Coder :> es) => Eff es Word32
-nextMT19937 = send NextMT19937
-
-codeW32 :: Coder :> es => Word32 -> Eff es Word32
-codeW32 w32 = do
-    rng1 <- nextMT19937
-    rng2 <- nextMT19937
-    let cipher = complement (rng1 * rng2)
-    pure (cipher ^ w32)
-
-codeBlock :: Coder :> es => ByteString -> Eff es ()
-codeBlock block = do
-    let (w32Bs, block') = B.splitAt 4 block
-
--}
-
-{-
-
-type instance DispatchOf Coder = Dynamic
-data Coder :: Effect where
-    NextW32     :: Coder m Word32
-    NextMT19937 :: Coder m Word32
-    WriteW32    :: Word32 -> Coder m ()
-
-nextW32 :: (HasCallStack, Coder :> es) => Eff es Word32
-nextW32 = send Next32
-
-writeW32 :: (HasCallStack, Coder :> es) => Word32 -> Eff es ()
-writeW32 w32 = send (Write32 w32)
-
-codeW32 :: Coder :> es => Eff es ()
-codeW32 = do
-    w32 <- nextW32
-    rng1 <- nextMT19937
-    rng2 <- nextMT19937
-    let cipher = complement(rng1 * rng2)
-    writeW32 (cipher ^ w32)
-
-code :: Eff es ()
-code = ifM more (codeW32 *> code) (pure ())
-
--- | Like @if@, but where the test can be monadic. (from extra)
-ifM :: Monad m => m Bool -> m a -> m a -> m a
-ifM b t f = do b <- b; if b then t else f
-
--}
