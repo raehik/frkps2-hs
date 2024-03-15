936127997cfd93cfb200394c9d80ee71193d5c7c
Ben Orchard <thefirstmuffinman@gmail.com>
2024-03-15 18:18:55 +0000

    FRK2: split internal bits out

--- src/FrkPs2/FRK2.hs
+++ src/FrkPs2/FRK2.hs
@@ -9,14 +9,6 @@ Conveniently, this means the same operation both encodes and decodes. Indeed:
 
 module FrkPs2.FRK2 where
 
-import MT19937.Pure qualified as MT19937
-import MT19937.Pure ( MT19937 )
-import Data.Bits ( xor, complement )
-import Data.Word
-
-import Control.Monad.Primitive ( PrimMonad )
-import Data.Primitive.Ptr ( readOffPtr, writeOffPtr )
-import Foreign.Ptr ( Ptr, castPtr )
 import GHC.ForeignPtr ( unsafeWithForeignPtr )
 import Foreign.Marshal.Utils ( copyBytes )
 
@@ -26,61 +18,32 @@ import Data.ByteString          qualified as B
 import System.IO ( Handle, hGetBuf, hPutBuf )
 import Foreign.Marshal.Alloc ( allocaBytes )
 
-initialPrng :: MT19937
-initialPrng = MT19937.skip 1 $ MT19937.init 0xFACEFACE
-
--- | Encodes or decodes.
---
--- @code rng w (fst (code rng w)) === w@
-codeW32 :: MT19937 -> Word32 -> (Word32, MT19937)
-codeW32 prng0 w = (w `xor` cipher, prng2)
-  where
-    cipher = complement (rnd1 * rnd2)
-    (rnd1, prng1) = MT19937.extract prng0
-    (rnd2, prng2) = MT19937.extract prng1
+import FrkPs2.FRK2.Internal ( codeBuf, initialPrng )
 
 -- can we use unsafeWithForeignPtr ? idk probably
 code :: B.ByteString -> B.ByteString
 code (B.BS fptr len) =
     B.unsafeCreate len $ \dest -> unsafeWithForeignPtr fptr $ \src -> do
         copyBytes dest src len
-        _prng' <- code' dest len initialPrng
+        _prng' <- codeBuf dest len initialPrng
         pure ()
 
 codeFile :: FilePath -> IO B.ByteString
 codeFile fp = code <$> B.readFile fp
 
+-- | Code from one handle to another, using the given buffer size.
+--
+-- I don't know what a good buffer size is for this stuff. So _you_ decide! >:)
 codeHandle :: Int -> Handle -> Handle -> IO ()
 codeHandle bufLen hr hw = allocaBytes bufLen (go initialPrng)
   where
-    go :: MT19937 -> Ptr Word8 -> IO ()
     go prng buf = do
         bufLenWritten <- hGetBuf hr buf bufLen
         if   bufLenWritten < bufLen
-        then do _prng' <- code' buf bufLenWritten prng
+        then do _prng' <- codeBuf buf bufLenWritten prng
                 hPutBuf hw buf bufLenWritten
                 pure ()
-        else do prng' <- code' buf bufLenWritten prng
+        else do prng' <- codeBuf buf bufLenWritten prng
                 -- safely assume bufLen == bufLenWritten so either is OK
                 hPutBuf hw buf bufLenWritten
                 go prng' buf
-
--- buf is mutable data
--- taking Ptr because looks like copy prims work on them (see GHC.IO.Handle)
--- maybe have to worry about the casts if alignment matters :(
--- but it's surely fine it wouldn't make sense!
-code'
-    :: PrimMonad m
-    => Ptr Word8 -> Int -> MT19937 -> m MT19937
-code' buf len prng0 = goW32 prng0 0
-  where
-    bufW32 :: Ptr Word32 = castPtr buf
-    (w32s, _w8s) = len `quotRem` 4
-    goW32 prng i
-      | i == w32s = goW8s prng (0 :: Int)
-      | otherwise = do
-            w32 <- readOffPtr bufW32 i
-            let (w32Coded, prng') = codeW32 prng w32
-            writeOffPtr bufW32 i w32Coded
-            goW32 prng' (i+1)
-    goW8s prng _i = pure prng -- TODO idk how to handle these exactly
---
+++ src/FrkPs2/FRK2/Internal.hs
@@ -0,0 +1,49 @@
+-- | General FRK2 bits.
+
+module FrkPs2.FRK2.Internal where
+
+import MT19937.Pure qualified as MT19937
+import MT19937.Pure ( MT19937 )
+import Data.Bits ( xor, complement )
+import Data.Word
+
+import Control.Monad.Primitive ( PrimMonad )
+import Data.Primitive.Ptr ( readOffPtr, writeOffPtr )
+import Foreign.Ptr ( Ptr, castPtr )
+
+initialPrng :: MT19937
+initialPrng = MT19937.skip 1 $ MT19937.init 0xFACEFACE
+
+{- | Code the data in a mutable buffer given its length and the current MT19937
+     PRNG state. Returns the next PRNG state, and has changed the buffer.
+
+Codes in 32-bit words. Ignores any final bytes if buffer isn't a multiple of 4.
+
+This can be used to implement efficient coding for any interface (bytestrings,
+handles, probably sockets too).
+-}
+codeBuf
+    :: PrimMonad m
+    => Ptr Word8 -> Int -> MT19937 -> m MT19937
+codeBuf buf len prng0 = goW32 prng0 0
+  where
+    bufW32 :: Ptr Word32 = castPtr buf
+    (w32s, _w8s) = len `quotRem` 4
+    goW32 prng i
+      | i == w32s = goW8s prng (0 :: Int)
+      | otherwise = do
+            w32 <- readOffPtr bufW32 i
+            let (w32Coded, prng') = codeW32 prng w32
+            writeOffPtr bufW32 i w32Coded
+            goW32 prng' (i+1)
+    goW8s prng _i = pure prng -- TODO idk how to handle these exactly
+
+-- | Code the given word.
+--
+-- @code rng w (fst (code rng w)) === w@
+codeW32 :: MT19937 -> Word32 -> (Word32, MT19937)
+codeW32 prng0 w = (w `xor` cipher, prng2)
+  where
+    cipher = complement (rnd1 * rnd2)
+    (rnd1, prng1) = MT19937.extract prng0
+    (rnd2, prng2) = MT19937.extract prng1
